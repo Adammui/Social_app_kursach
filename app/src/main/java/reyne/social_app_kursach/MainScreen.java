9dfbf7ff05e2efc7eead55e1a665e1d91dfea88a
Adammui <adami.yuje@gmail.com>
2022-04-30 13:29:04 +0300

    designing userpage (litl fix for broken fragments)

--- app/src/main/java/reyne/social_app_kursach/MainScreen.java
+++ app/src/main/java/reyne/social_app_kursach/MainScreen.java
@@ -29,9 +29,9 @@ public class MainScreen extends AppCompatActivity {
         //AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
          //       R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
          //       .build();
-        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main_screen);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main_screen);
         //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-       // NavigationUI.setupWithNavController(binding.navView, navController);
+        NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
 }
